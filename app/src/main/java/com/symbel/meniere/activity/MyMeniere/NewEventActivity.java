ed47546f32c41ad895fe955857dd3806c235c062
DESKTOP-VMGG9DP\stefy83 <stefy83@gmail.com>
2018-01-26 19:37:24 +0100

    custom seekbar

---
+++ app/src/main/java/com/symbel/meniere/activity/MyMeniere/NewEventActivity.java
@@ -0,0 +1,17 @@
+package com.symbel.meniere.activity.MyMeniere;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.symbel.meniere.R;
+
+public class NewEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_event);
+    }
+
+
+}
--- app/src/main/java/com/symbel/meniere/activity/MyMeniere/OneFragment.java
+++ app/src/main/java/com/symbel/meniere/activity/MyMeniere/OneFragment.java
@@ -1,15 +1,28 @@
 package com.symbel.meniere.activity.MyMeniere;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 import com.symbel.meniere.R;
+import com.symbel.meniere.activity.HelpMeniere.AuthorsActivity;
+import com.symbel.meniere.activity.TabsActivity;
+
+import java.util.Date;
 
 
 public class OneFragment extends Fragment {
+
+    private View rootView;
+    private CalendarView mCalendarV;
+    private CardView mFocusTodayCv;
+    private CardView mNewEventCv;
 
     public OneFragment() {
         // Required empty public constructor
@@ -19,13 +32,48 @@ public class OneFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false);
+        rootView = inflater.inflate(R.layout.fragment_one, container, false);
+        return rootView;
+    }
+
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        try {
+
+            mFocusTodayCv = (CardView) rootView.findViewById(R.id.cvFocusToday);
+            mNewEventCv = (CardView) rootView.findViewById(R.id.cvNewEvent);
+
+            mFocusTodayCv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    focusToday();
+                }
+            });
+
+            mNewEventCv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    TabsActivity.activitySwitchFlag = true;
+                    startActivity(new Intent(getActivity(), NewEventActivity.class));
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void focusToday(){
+        mCalendarV = (CalendarView) rootView.findViewById(R.id.calendarView);
+        mCalendarV.setDate(new Date().getTime());
+
     }
 
 }
