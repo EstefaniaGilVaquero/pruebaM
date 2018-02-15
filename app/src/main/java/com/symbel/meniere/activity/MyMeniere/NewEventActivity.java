cd57b6dc2569c6e0fcb3abe21da856e30fcd8a86
DESKTOP-VMGG9DP\stefy83 <stefy83@gmail.com>
2018-02-15 18:55:46 +0100

    strings de la vista nuevo evento

--- app/src/main/java/com/symbel/meniere/activity/MyMeniere/NewEventActivity.java
+++ app/src/main/java/com/symbel/meniere/activity/MyMeniere/NewEventActivity.java
@@ -2,15 +2,28 @@ package com.symbel.meniere.activity.MyMeniere;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
 import com.symbel.meniere.R;
 
 public class NewEventActivity extends AppCompatActivity {
 
+    private View mScrollView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_event);
+
+        mScrollView = findViewById(R.id.newEventScrollView);
+        mScrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+            @Override
+            public void onScrollChanged() {
+                // call this method to correct offsets
+                //mBubbleSeekBar.correctOffsetWhenContainerOnScrolling();
+            }
+        });
     }
 
 
